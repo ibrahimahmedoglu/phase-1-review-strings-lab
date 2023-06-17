4fc15fb8af00c78067c3bb8e576a88c007bc1e3b
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-17 14:41:10 +0300

    Completed Assigntment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = 'John Doe';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+// Export the variables
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
